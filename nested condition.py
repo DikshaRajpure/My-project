3e2f90b8f2908cb004535e1dcd9efa5b7221b27d
diksha rajpure <diksharajpure619@gmail.com>
2025-05-30 11:00:04 +0530

    first commit

---
+++ nested condition.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 24 09:31:12 2025
+
+@author: ADMIN
+"""
+
+age=int(input("enter age"))
+if age<12:
+    print("you are child")
+elif age<19:
+    print("your are teenager")
+elif age<35:
+    print("your are young")
+else:
+    print("you are adult")
+    
+        
