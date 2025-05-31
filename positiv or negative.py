45ea76aece50b93efc4d5021d902d0f719c64660
diksha rajpure <diksharajpure619@gmail.com>
2025-05-31 10:47:05 +0530

    Add files via upload

---
+++ positiv or negative.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 24 09:23:00 2025
+
+@author: ADMIN
+"""
+
+num=int(input("enter number"))
+if num<0:
+    print("no is negative")
+else:
+    print("no is positive")
